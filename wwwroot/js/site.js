0be31f99328fa929a6687f33a1eb272dc195abb1
NikaGogelia <gogelianika1@gmail.com>
2023-10-12 17:07:19 +0400

    add sidebar active link state, add styles for sidebar links

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿"use strict"
 
-// Write your JavaScript code.
+// --------- Sidebar Active State --------- //
+const routeLocation = window.location.href.split("/")[3];
+const sidebarLinks = document.querySelectorAll(".sidebar-navlink");
+
+sidebarLinks.forEach((sidebarLink) => {
+    sidebarLink.addEventListener("click", () => {
+        localStorage.setItem("routeLocation", sidebarLink.dataset.name);
+    })
+})
+
+let chosenLocation = null;
+
+if (chosenLocation === null) {
+    localStorage.setItem("routeLocation", routeLocation);
+    chosenLocation = localStorage.getItem("routeLocation");
+} else {
+    chosenLocation = localStorage.getItem("routeLocation");
+}
+
+if (chosenLocation === routeLocation) {
+    const chosenElement = document.querySelector(`[data-name="${chosenLocation}"]`)
+    chosenElement.classList.add("active-navlink");
+}
+// --------- Sidebar Active State --------- //
