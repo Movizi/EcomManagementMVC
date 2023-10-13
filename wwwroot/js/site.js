d28b7530b639c7fa4840a561246f99c310b7d35a
NikaGogelia <gogelianika1@gmail.com>
2023-10-13 16:50:15 +0400

    add sidebar toggler, add header

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -24,3 +24,31 @@ if (chosenLocation === routeLocation) {
     chosenElement.classList.add("active-navlink");
 }
 // --------- Sidebar Active State --------- //
+
+
+// --------- Sidebar Toggler --------- //
+const sidebar = document.querySelector(".sidebar");
+const sidebarToggler = document.querySelector(".sidebar-toggler");
+const togglerArrow = document.querySelector(".toggler-arrow");
+let togglerActive = JSON.parse(localStorage.getItem("togglerActive")) || false;
+
+updateSidebar();
+
+sidebarToggler.addEventListener("click", () => {
+    togglerActive = !togglerActive;
+    localStorage.setItem("togglerActive", JSON.stringify(togglerActive));
+    updateSidebar();
+});
+
+function updateSidebar() {
+    if (togglerActive) {
+        togglerArrow.classList.add("turn-round");
+        sidebar.classList.remove("long-sidebar");
+        sidebar.classList.add("short-sidebar")
+    } else {
+        togglerArrow.classList.remove("turn-round");
+        sidebar.classList.remove("short-sidebar");
+        sidebar.classList.add("long-sidebar");
+    }
+}
+// --------- Sidebar Toggler --------- //
